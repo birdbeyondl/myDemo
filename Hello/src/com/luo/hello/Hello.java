9c960799f4f6c7fd2d3eee0238361c542be0e12c
Dillan.Luo <birdbeyondl@126.com>
2013-05-15 23:31:49 +0800

    demo

---
+++ Hello/src/com/luo/hello/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.luo.hello;
+
+/**
+ * @author birdbeyondl
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
